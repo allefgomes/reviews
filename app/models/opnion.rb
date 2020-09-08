ebae6f4c0035fc28fdc55606ffa9f947f777a541
Allef Gomes <allefgalmeida@gmail.com>
2020-09-08 19:06:47 -0300

    add range to rating

--- app/models/opnion.rb
+++ app/models/opnion.rb
@@ -5,4 +5,5 @@ class Opnion < ApplicationRecord
   belongs_to :product
 
   validates :timestamp, :rating, :review_text, :product, presence: true
+  validates :rating, inclusion: { in: 0..10, message: 'Validation failed: Rating need between 1 and 10.' }
 end
