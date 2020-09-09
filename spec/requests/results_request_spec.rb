f543cfa8fa0ae0660affe3dfa3d0cac9aa15c40d
Allef Gomes <allefgalmeida@gmail.com>
2020-09-09 10:32:12 -0300

    return 404 test url

--- spec/requests/results_request_spec.rb
+++ spec/requests/results_request_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe 'Results', type: :request do
       pro = Product.create(name: 'Name Test')
       pro.opnions.create(timestamp: 123, rating: 1, review_text: Faker::Lorem.sentence)
 
-      get '/results/Name%20Test'
+      get '/results/Name_Test'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http not_found' do
+      get '/results/not_found_product'
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
